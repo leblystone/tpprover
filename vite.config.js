72cbd6f5875c22f480a9ca4f945c4bb4924768b1
leblystone <your-email@example.com>
2025-09-07 21:14:19 -0600

    Fix development environment warnings and errors

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3006,
+    host: true
+  },
+  define: {
+    'process.env': {},
+    'global': 'globalThis'
+  },
   build: {
     rollupOptions: {
       external: [
